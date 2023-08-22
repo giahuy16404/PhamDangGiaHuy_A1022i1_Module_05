41dd599d78364aa65e3567e23b2aac9e1bc070e5
giahuy16404 <phamhuy1642004@gmail.com>
2023-08-22 17:53:11 +0700

    ss04_hook

--- Homework/ss04_hook/counter/src/hook/useIncrement.js
+++ Homework/ss04_hook/counter/src/hook/useIncrement.js
@@ -1,7 +1,12 @@
 import {useState} from "react";
 
-function useIncrement() {
-    const [count, setCount] = useState(0)
+function useIncrement(valueDefault) {
+    if (!valueDefault) {
+        valueDefault = 0;
+    }
+
+    const [count, setCount] = useState(valueDefault)
+    console.log(count)
 
     const increment = (value) => {
         setCount((prev) => prev + value)
