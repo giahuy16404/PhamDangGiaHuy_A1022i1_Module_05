a9510260c38d7b575acd597f9b18543779477b55
giahuy16404 <phamhuy1642004@gmail.com>
2023-08-22 16:50:04 +0700

    ss04_Hook

--- Homework/ss04_hook/counter/src/hook/useIncrement.js
+++ Homework/ss04_hook/counter/src/hook/useIncrement.js
@@ -4,7 +4,7 @@ function useIncrement(value) {
     const [count, setCount] = useState(value)
 
     const increment = () => {
-        setCount((prev) => +prev + value)
+        setCount((prev) => prev + value)
     }
     return [count, increment];
 }
